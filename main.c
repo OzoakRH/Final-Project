df0f5edad23cc4bf1a8f923ee4fffdc8868ed3bf
OzoakRH <kanekineki50@gmail.com>
2025-10-08 02:08:24 +0700

    Update main.c

--- main.c
+++ main.c
@@ -10,3 +10,17 @@ typedef struct {
     char seminarDate[20];
     int participantsCount;
 } Seminar;
+
+void open_file() {
+    FILE *file = fopen(FILE_NAME, "a+");
+    if (!file) {
+        perror("Error opening file");
+        exit(1);
+    }
+    fseek(file, 0, SEEK_END);
+    if (ftell(file) == 0) {
+        fprintf(file, "ParticipantName,SeminarTitle,SeminarDate,ParticipantsCount\n");
+    }
+    fclose(file);
+}
+
